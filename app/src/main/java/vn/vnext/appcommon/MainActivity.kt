973f51cf2f24015fd1e5d2f9e05d87508a4dccaa
Nguyen Dinh Kien <kiennd@vnext.vn>
2022-05-12 16:28:47 +0700

    implement SharedPreferences modules

--- app/src/main/java/vn/vnext/appcommon/MainActivity.kt
+++ app/src/main/java/vn/vnext/appcommon/MainActivity.kt
@@ -2,13 +2,18 @@ package vn.vnext.appcommon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import dagger.hilt.android.AndroidEntryPoint
+import vn.vnext.appcommon.domain.preferences.PrefsHelper
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/vn/vnext/appcommon/domain/preferences/PrefsHelper.kt
@@ -0,0 +1,18 @@
+package vn.vnext.appcommon.domain.preferences
+
+interface PrefsHelper {
+    fun saveToString(prefName: String, prefValue: String)
+
+
+    fun saveToBoolean(prefName: String, prefValue: Boolean)
+
+    fun saveToInteger(prefName: String, prefValue: Int)
+
+    fun readString(prefName: String, defValue: String): String
+
+    fun readBoolean(prefName: String, defValue: Boolean): Boolean
+
+    fun readInteger(prefName: String, defValue: Int): Int
+
+    fun remove(prefName: String)
+}
